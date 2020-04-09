76aff910bae5581f1d4ac3d47c5c2c5a5a7ace05
Max Hermans <maxhermans@live.com>
2020-04-09 10:29:45 +0200

    python files for new data splits

--- import_new_dataset.py
+++ import_new_dataset.py
@@ -1,5 +1,14 @@
+'''
+import pandas as pd
+metadata_pd = pd.read_csv('./openaccess/MetObjects.csv') 
+ 
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(metadata_pd.head())
+
+'''
+
 import requests
 
-response = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/departments")
 
-print(response)
+response = requests.get("http://collectionapi.metmuseum.org/public/collection/v1/objects/")
+print(response.status_code)
